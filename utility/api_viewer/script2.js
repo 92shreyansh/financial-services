0dfb4cddd516d214d607880abf50757848a387f8
92shreyansh <shreyansh.p.shah@gmail.com>
2023-05-29 15:51:07 +0530

    commit

--- utility/api_viewer/script2.js
+++ utility/api_viewer/script2.js
@@ -2,106 +2,109 @@
 
 // main.js
 
-  function fetchData(url) {
-    return fetch(url)
-      .then(response => response.text())
-      .then(yamlData => jsyaml.load(yamlData));
-  }
-  // Example usage: fetch YAML data from a URL
-  fetchData('https://raw.githubusercontent.com/92shreyansh/json-schema-store/v1.0.0/enum.yaml')
-    .then(data => {
-      console.log(data)
-      parsedData = data
-      loadDropdown1()
-      loadDropdown2()
-      loadDropdown3()
-    })
-    .catch(error => {
-      console.error('Error fetching or parsing YAML:', error);
-    });
+function loadDropdown1() {
+var dropdown1 = document.getElementById('dropdown1');
+dropdown1.innerHTML = '';
 
+// Populate dropdown1
+Object.keys(parsedData).forEach(function(key) {
+  var option = document.createElement('option');
+  option.text = key;
+  dropdown1.add(option);
+});
+}
+
+function loadDropdown2() {
+var dropdown1 = document.getElementById('dropdown1');
+var dropdown2 = document.getElementById('dropdown2');
+dropdown2.innerHTML = '';
+
+// Get the selected value from dropdown1
+var selectedValue1 = dropdown1.value;
+
+// Populate dropdown2
+Object.keys(parsedData[selectedValue1]).forEach(function(key) {
+  var option = document.createElement('option');
+  option.text = key;
+  dropdown2.add(option);
+});
+}
+
+function loadDropdown3() {
+var dropdown1 = document.getElementById('dropdown1');
+var dropdown2 = document.getElementById('dropdown2');
+var dropdown3 = document.getElementById('dropdown3');
+dropdown3.innerHTML = '';
+
+// Get the selected values from dropdown1 and dropdown2
+var selectedValue1 = dropdown1.value;
+var selectedValue2 = dropdown2.value;
+
+// Populate dropdown3
+parsedData[selectedValue1][selectedValue2].forEach(function(item) {
+  var option = document.createElement('option');
+  option.text = item["value"];
+  console.log("iten added", item.value)
+  dropdown3.add(option);
+});
+}
+
+function displayTable() {
+  var dropdown1 = document.getElementById('dropdown1');
+  var dropdown2 = document.getElementById('dropdown2');
+  var dropdown3 = document.getElementById('dropdown3');
+
+  // Get the selected values from dropdown1, dropdown2, and dropdown3
+  var selectedValue1 = dropdown1.value;
+  var selectedValue2 = dropdown2.value;
+  var selectedValue3 = dropdown3.value;
+
+  console.log("selectedValue3", selectedValue3)
+  // Get the table data
+  var tableData = parsedData[selectedValue1][selectedValue2].find(obj => {
+      if(obj["value"] == selectedValue3)
+          return obj
+  });
+
+  // Get the table body element
+  var tableBody = document.getElementById('result-table');
+  if(tableBody && tableBody != {})tableBody.innerHTML = '';
+  insertRow(tableBody, "ENUM", tableData.value)
+  insertRow(tableBody, "Description", tableData.description)
+  insertRow(tableBody, "Refrences", tableData.reference)
+  insertRow(tableBody, "APIs", tableData.api)
   
-    function loadDropdown1() {
-      var dropdown1 = document.getElementById('dropdown1');
-      dropdown1.innerHTML = '';
+}
 
-      // Populate dropdown1
-      Object.keys(parsedData).forEach(function(key) {
-        var option = document.createElement('option');
-        option.text = key;
-        dropdown1.add(option);
-      });
-    }
+function insertRow(tableBody, key, value){
+  var row = tableBody.insertRow();
+  var cell = row.insertCell();
+  cell.innerHTML = key
+  var cell = row.insertCell();
+  cell.innerHTML = value;
+}
 
-    function loadDropdown2() {
-      var dropdown1 = document.getElementById('dropdown1');
-      var dropdown2 = document.getElementById('dropdown2');
-      dropdown2.innerHTML = '';
+function fetchData(url) {
+  return fetch(url)
+    .then(response => response.text())
+    .then(yamlData => jsyaml.load(yamlData));
+}
 
-      // Get the selected value from dropdown1
-      var selectedValue1 = dropdown1.value;
-      
-      // Populate dropdown2
-      Object.keys(parsedData[selectedValue1]).forEach(function(key) {
-        var option = document.createElement('option');
-        option.text = key;
-        dropdown2.add(option);
-      });
-    }
+// Example usage: fetch YAML data from a URL
+fetchData('https://raw.githubusercontent.com/92shreyansh/json-schema-store/v1.0.0/enum.yaml')
+  .then(data => {
+    console.log(data)
+    parsedData = data
+    loadDropdown1()
+    loadDropdown2()
+    loadDropdown3()
+  })
+  .catch(error => {
+    console.error('Error fetching or parsing YAML:', error);
+  });
 
-    function loadDropdown3() {
-      var dropdown1 = document.getElementById('dropdown1');
-      var dropdown2 = document.getElementById('dropdown2');
-      var dropdown3 = document.getElementById('dropdown3');
-      dropdown3.innerHTML = '';
 
-      // Get the selected values from dropdown1 and dropdown2
-      var selectedValue1 = dropdown1.value;
-      var selectedValue2 = dropdown2.value;
 
-      // Populate dropdown3
-      parsedData[selectedValue1][selectedValue2].forEach(function(item) {
-        var option = document.createElement('option');
-        option.text = item["value"];
-        console.log("iten added", item.value)
-        dropdown3.add(option);
-      });
-    }
 
-    function displayTable() {
-        var dropdown1 = document.getElementById('dropdown1');
-        var dropdown2 = document.getElementById('dropdown2');
-        var dropdown3 = document.getElementById('dropdown3');
-      
-        // Get the selected values from dropdown1, dropdown2, and dropdown3
-        var selectedValue1 = dropdown1.value;
-        var selectedValue2 = dropdown2.value;
-        var selectedValue3 = dropdown3.value;
-      
-        console.log("selectedValue3", selectedValue3)
-        // Get the table data
-        var tableData = parsedData[selectedValue1][selectedValue2].find(obj => {
-            if(obj["value"] == selectedValue3)
-                return obj
-        });
-      
-        // Get the table body element
-        var tableBody = document.getElementById('result-table');
-        if(tableBody && tableBody != {})tableBody.innerHTML = '';
-        insertRow(tableBody, "ENUM", tableData.value)
-        insertRow(tableBody, "Description", tableData.description)
-        insertRow(tableBody, "Refrences", tableData.reference)
-        insertRow(tableBody, "APIs", tableData.api)
-        
-      }
-
-      function insertRow(tableBody, key, value){
-        var row = tableBody.insertRow();
-        var cell = row.insertCell();
-        cell.innerHTML = key
-        var cell = row.insertCell();
-        cell.innerHTML = value;
-      }
-
-      
-      
+    
+    
--- utility/example_util/app.js
+++ utility/example_util/app.js
@@ -1,25 +1,67 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const $RefParser = require('json-schema-ref-parser');
+const { execSync } = require('child_process');
+const { promisify } = require('util');
 
 const args = process.argv.slice(2);
-var example_set = args[0]
-var baseYaml = args[1]
-var result = args[2]
-const examples_yaml = fs.readFileSync(`${example_set}`, 'utf8');
-const examples = yaml.load(examples_yaml);
-const spec_yaml = fs.readFileSync(baseYaml, 'utf8');
-const spec = yaml.load(spec_yaml);
+var module_name = args[0];
+var example_set = args[1];
 
-// console.log(examples)
+if (!fs.existsSync(`api/${module_name}`)) {
+  console.error('[ERROR] Module does not exist');
+  process.exit(1);
+}
 
-for(var key in examples){
-    var list = examples[key]["examples"]
-    spec["paths"]["/"+key]["post"]["requestBody"]["content"]["application/json"]["examples"] = {};
-    for(var key2 in list){
-        spec["paths"]["/"+key]["post"]["requestBody"]["content"]["application/json"]["examples"]["e"+key2] = list[key2];
+
+
+const tempPath = `api/${module_name}/components/temp.yaml`;
+const outputPath = `api/${module_name}/build/${module_name}.yaml`;
+const unresolvedFilePath = `https://raw.githubusercontent.com/beckn/protocol-specifications/master/api/transaction/components/index.yaml`
+// const spec = yaml.load(unresolvedFilePath);
+
+$RefParser.dereference(unresolvedFilePath)
+  .then((schema) => {
+    spec = schema;
+    return $RefParser.dereference(example_set)
+  })
+  .then((schema) => {
+    examples = schema;
+    GenerateYaml(spec, examples, tempPath);
+    buildSwagger(tempPath, outputPath);
+    cleanup()
+  })
+  .catch((error) => {
+    console.error('Error parsing schema:', error);
+  });
+
+function cleanup() {
+  try {
+    fs.unlinkSync(tempPath);
+    console.log('Temporary file deleted');
+  } catch (error) {
+    console.error('Error deleting temporary file:', error);
+  }
+}
+
+function buildSwagger(inPath, outPath) {
+  try {
+    const command = `swagger-cli bundle ${inPath} --outfile ${outPath} -t yaml`;
+    execSync(command, { stdio: 'inherit' });
+  } catch (error) {
+    console.error('An error occurred while generating the Swagger bundle:', error);
+    process.exit(1);
+  }
+}
+
+function GenerateYaml(base, examples, output_yaml) {
+  for (var key in examples) {
+    var list = examples[key]["examples"];
+    base["paths"]["/" + key]["post"]["requestBody"]["content"]["application/json"]["examples"] = {};
+    for (var key2 in list) {
+      base["paths"]["/" + key]["post"]["requestBody"]["content"]["application/json"]["examples"]["e" + key2] = list[key2];
     }
-} 
-
-const output_yaml = yaml.dump(spec);
-
-fs.writeFileSync(result, output_yaml, 'utf8');
+  }
+  const output = yaml.dump(base);
+  fs.writeFileSync(output_yaml, output, 'utf8');
+}
